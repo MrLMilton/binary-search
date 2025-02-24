49ab4326302d15b584cb9921e3dd1881efd8e5b1
Lang Milton <lmilton@heckgrammar.co.uk>
2025-02-24 10:00:25 +0000

    started

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int upper = ints.GetUpperBound(0);
+            int lower = 0;
+            Console.WriteLine("what number are you looking for? ");
+            int target = Convert.ToInt32(Console.ReadLine);
+            int mid;
+            bool found = false;
+            while (lower < upper && found == false )
+            {
+                mid = (lower + upper) / 2;
+                if (target == ints[mid])
+                {
+                    found = true;
+                }
+            }
         }
     }
 }
