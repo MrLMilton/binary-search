5bb931eae434e6d38919eb860d0937382561e2c7
Lang Milton <lmilton@heckgrammar.co.uk>
2025-02-25 09:27:20 +0000

    completed

--- Program.cs
+++ Program.cs
@@ -8,16 +8,33 @@
             int upper = ints.GetUpperBound(0);
             int lower = 0;
             Console.WriteLine("what number are you looking for? ");
-            int target = Convert.ToInt32(Console.ReadLine);
+            int target = Convert.ToInt32(Console.ReadLine());
             int mid;
             bool found = false;
-            while (lower < upper && found == false )
+            while (lower <= upper && found == false )
             {
                 mid = (lower + upper) / 2;
                 if (target == ints[mid])
                 {
                     found = true;
+                    Console.WriteLine(mid);
                 }
+                else if (target < ints[mid])
+                {
+                    upper = mid - 1;
+                }
+                else 
+                {
+                    lower = mid + 1;
+                }
+            }
+            if (found == true)
+            {
+                Console.WriteLine("Target found");
+            }
+            else 
+            { 
+                Console.WriteLine("Target not found");
             }
         }
     }
